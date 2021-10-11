1745018f5f54ea89b7d0ee76c343c8ce09ce7334
farwaali08 <farwaali@yahoo.com>
2021-10-11 17:35:20 -0400

    push

--- schema.sql
+++ schema.sql
@@ -48,15 +48,13 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 PRIMARY KEY (emp_no)
 );
-
---Creating Titles table
+-- Creating Titles tables
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	title VARCHAR NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
 );
 
 -- quick query using SELECT statement
